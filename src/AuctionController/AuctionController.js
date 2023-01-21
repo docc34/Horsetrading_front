5ad257d683c274d81cb611a718a27071863695ec
docc34 <eemeli.antikainen@gmail.com>
2023-01-21 19:04:35 +0200

    Säädin datetimen kaa

--- src/AuctionController/AuctionController.js
+++ src/AuctionController/AuctionController.js
@@ -8,7 +8,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import {handleInputChange} from  '../functions'
-
+import DateFilter from '@inovua/reactdatagrid-community/DateFilter'
+import moment from 'moment'
 
 const AuctionController = ()=>{
     const [loggedIn, setLoggedIn] = useState(false);
@@ -23,20 +24,59 @@ const AuctionController = ()=>{
 
     const [cookies, setCookie,removeCookie] = useCookies(['token']);
     
+    const [auctionItemPostModal, setAuctionItemPostModal] = useState(false);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [closingTime, setClosingTime] = useState("");
+    
     const columns = [
         {name:"id", header: "Id",  defaultVisible: false},
         {name:"title" , header:"Title",  defaultFlex:2},
         {name:"description" , header:"Description", defaultFlex:2},
         {name:"active" , header:"Active", defaultFlex:1},
-        {name:"closingTime" , header:"Closing Time", type: "number", defaultFlex:2},
+        {
+            name: 'closingTime',header: 'Closing Time',defaultWidth: 150,
+            // need to specify dateFormat
+            dateFormat: 'YYYY-MM-DD, HH:mm',
+            filterEditor: DateFilter,
+            filterEditorProps: (props, { index }) => {
+              // for range and notinrange operators, the index is 1 for the after field
+              return {
+                dateFormat: 'YYYY-MM-DD, HH:mm',
+                placeholder: index == 1 ? 'Last login date is before...': 'Last login date is after...'
+              }
+            },
+            //Momentjs formats the date
+            render: ({ value, cellProps: { dateFormat } }) =>
+              moment(value).format(dateFormat),
+        },
+        //{name: 'closingTime',header: 'Closing Time', defaultFlex:2,},
         {name:"url" , header:"Url", defaultFlex:2, render: ({value}) => {
             return <a href={value}>Auction</a>
         }}
     ]
+    // {
+    //     name: 'created',header: 'Created',defaultWidth: 150,
+    //     // need to specify dateFormat
+    //     dateFormat: 'YYYY-MM-DD, HH:mm',
+    //     filterEditor: DateFilter,
+    //     filterEditorProps: (props, { index }) => {
+    //       // for range and notinrange operators, the index is 1 for the after field
+    //       return {
+    //         dateFormat: 'YYYY-MM-DD, HH:mm',
+    //         placeholder: index == 1 ? 'Last login date is before...': 'Last login date is after...'
+    //       }
+    //     },
+    //     //Momentjs formats the date
+    //     render: ({ value, cellProps: { dateFormat } }) =>
+    //       moment(value).format(dateFormat),
+    // },
     const resetValues = ()=>{
         setPassword("");
         setUsername("");
         setMessage("");
+        setShow(false);
+        setAuctionItemPostModal(false);
     }
 
     const handleLogin = async() => {
@@ -83,11 +123,25 @@ const AuctionController = ()=>{
         }
     }
 
+    const postAuctionItem = ()=>{
+        if(title != "" && description != "" && closingTime != ""){
+            const options = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json',"Authorization": `Bearer ${cookies.token}`},
+                body:JSON.stringify({
+                    Title: title,
+                    Description:description,
+                    ClosingTime: closingTime,
+                    Active: 1
+                })
+            }
+        }
+    }
     
     const fetchAuctionItems = async ()=>{
         const options = {
             method: 'GET',
-            headers: { "Authorization": `Bearer ${cookies.token}`}
+            headers: {"Authorization": `Bearer ${cookies.token}`}
         }
         var search = await fetch("https://localhost:44371/api/AuctionItems",options);
         var auctionItems = await search.json();
@@ -110,6 +164,8 @@ const AuctionController = ()=>{
     <div>
         {loggedIn == true ? 
             <div>
+                <h1>Auction controller</h1>
+                <a href="/">Home</a>
                 <div>
                     <ReactDataGrid
                         idProperty="id"
@@ -124,9 +180,54 @@ const AuctionController = ()=>{
                         />
                 </div>
                 <div>
-                <Button variant="primary" onClick={()=>{removeCookie('token',{ path: '/' });}}>
-                    logout
-                </Button>
+                    <Button variant="primary" onClick={()=>{removeCookie('token',{ path: '/' });}}>
+                        logout
+                    </Button>
+
+                    <Button variant="primary" onClick={()=>{setAuctionItemPostModal(true);}}>
+                        Add auction item
+                    </Button>
+                    <div>
+                        <Modal show={auctionItemPostModal} >
+
+                            <Modal.Header closeButton>
+                                <Modal.Title>Add auction item</Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body>
+                            <div className='homeFormInputs'>
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control placeholder='Title' onChange={(e)=>{setTitle(handleInputChange(e));}} />
+                            </div>
+
+                            <div className='homeFormInputs'>
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control placeholder='Description' onChange={(e)=>{setDescription(handleInputChange(e));}} />
+                            </div>
+
+                            <div className='homeFormInputs'>
+                                <Form.Label>Closing time</Form.Label>
+                                <Form.Control placeholder='Description' onChange={(e)=>{setClosingTime(handleInputChange(e));}} />
+                                <Form.Text>
+                                    Aika jolloin huutokauppaus suljetaan
+                                </Form.Text>
+                            </div>
+
+                            </Modal.Body>
+                            
+                            <Modal.Footer>
+                            <p className='errorMessage'>{message}</p>
+                                <Button variant="secondary" onClick={()=>{resetValues()}}>
+                                    Close
+                                </Button>
+                                <Button variant="primary" onClick={()=>{postAuctionItem()}}>
+                                    Save
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </div>
+
+                
                 </div>
             </div>
             :
