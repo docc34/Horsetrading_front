9fd1e5c8d973771cd257dbbe23d84886969d5949
docc34 <eemeli.antikainen@gmail.com>
2023-01-31 21:20:59 +0200

    Added cell rendering on the home site

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,42 @@
-import './Home.css'
+import './Home.css';
+import {MakeStoreCell} from '../functions';
+import { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Home = ()=>{
+    const[data,setData] = useState([]);
 
+    const getCellData = async()=>{
+        var search = await fetch("https://localhost:44371/api/AuctionItems/public");
+        var data = await search.json();
+        if(data != null || data != undefined){
+            setData(data);
+        }
+    }
+
+    useEffect(()=>{
+        getCellData();
+    },[]);
+
+    const renderCells = data.map((x)=>{
+        return<MakeStoreCell data={x}/>
+    });
     return(<div>
         <p>Teen kotisivun myöhemmin</p>
+        <p>aa</p>
         <a href="/Auction?auctionId=1">Esimerkkin huutokauppa</a>
         <br/>
         <a href="/AuctionController">Huutokaupan hallintasivu</a>
+        { data == null || data?.length == 0 ? 
+                <div className='a'>
+                    <Spinner  animation="border" /> 
+                </div>
+            :
+                <div>
+                    {renderCells}
+                </div>
+            }
+        
     </div>);
 }
 
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
+import './functions.css';
+import Spinner from 'react-bootstrap/Spinner';
+import { relativeTimeRounding } from 'moment';
+
 const handleInputChange = (o)=>{
     if(o.target.value == null  || o.target.value == ""|| o.target.value == 0){
         o.target.setAttribute('style','border-color: #8f37aa; border-width: 2px')
@@ -9,4 +13,20 @@ const handleInputChange = (o)=>{
     }
 }
 
-export {handleInputChange}
+const MakeStoreCell = (d)=>{
+    console.log(d);
+
+    return(
+        <div className="storeCellMainDiv">
+            <div className='storeCellImgDiv'>
+                <img src={d.data?.imageLink}/>
+            </div>
+            <div>
+                <h4>{d.data?.title}</h4>
+                <p>{d.data?.description}</p>
+            </div>
+        </div>
+    )
+}
+
+export {handleInputChange, MakeStoreCell}
