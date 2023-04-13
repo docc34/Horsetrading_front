a605b371cb4e336f2817a07bd097bfc133d6b237
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-13 20:12:51 +0300

    Conditional rendering lisätty

--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -3,10 +3,16 @@ import { useLocation } from "react-router-dom";
 import { UserContainer } from "../components/UserContainer";
 import { useState, useEffect } from "react";
 import CardGroup from 'react-bootstrap/CardGroup';
+import { useCookies } from 'react-cookie';
+
 
 const UserProfile = () => {
     const search = useLocation().search;
     const auctionId = new URLSearchParams(search).get('userId');
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [cookies] = useCookies(['token']);
+
+
 
     const[containerData,setContainerData] = useState([]);
     const getContainerData = async()=>{
@@ -16,21 +22,56 @@ const UserProfile = () => {
             setContainerData(data);
         }
     }
+
+    //login
+    const checkLoginStatus = async()=>{
+        if(cookies.token != null){
+            const options = {
+                method: 'GET',
+                headers: { "Authorization": `Bearer ${cookies.token}`}
+            }
+            let data = await fetch("https://horsetradingapidev.azurewebsites.net/api/Login", options);
+            let loggedIn = await data.json();
+            if(loggedIn == true){
+                setLoggedIn(loggedIn);
+            }
+        }
+    }
+
+    
+
     useEffect(()=>{
         getContainerData();
+        checkLoginStatus();
     },[]);
+
 
     return (
         <div className='main-container'>
-            <div className='user-container'>
-                <CardGroup>
-                {
-                    containerData.filter(item => item.id == auctionId).map(id => {
-                        return <UserContainer user={id} />
-                    })
-                }
-                </CardGroup>
-            </div>
+            {loggedIn == true ? 
+                <div className='user-container'>
+
+                
+                    <a href="/AuctionController">Control Page</a>
+                    <CardGroup>
+                    {
+                        containerData.filter(item => item.id == auctionId).map(id => {
+                            return <UserContainer user={id} />
+                        })
+                    }
+                    </CardGroup>
+                </div>
+            :
+                <div className='user-container'>
+                    <CardGroup>
+                    {
+                        containerData.filter(item => item.id == auctionId).map(id => {
+                            return <UserContainer user={id} />
+                        })
+                    }
+                    </CardGroup>
+                </div>
+            }
         </div>
     )
 }
