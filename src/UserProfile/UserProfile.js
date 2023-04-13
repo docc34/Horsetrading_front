310938c20ca35cfded0b6c0d3a08cad7070c9648
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-13 19:24:50 +0300

    UserProfile näyttää kyseisen käyttäjän julkaisut

--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -1,11 +1,31 @@
 import { useLocation } from "react-router-dom";
+import { UserContainer } from "../components/UserContainer";
+import { useState, useEffect } from "react";
 
 const UserProfile = () => {
     const search = useLocation().search;
     const auctionId = new URLSearchParams(search).get('userId');
-    
+
+    const[containerData,setContainerData] = useState([]);
+    const getContainerData = async()=>{
+        var search = await fetch("https://horsetradingapidev.azurewebsites.net/api/AuctionItems/public");
+        var data = await search.json();
+        if(data != null || data != undefined){
+            setContainerData(data);
+        }
+    }
+    useEffect(()=>{
+        getContainerData();
+    },[]);
+
     return (
-        <div><h1>I'm profile {auctionId}</h1></div>
+        <div>
+            {
+                containerData.filter(item => item.id == auctionId).map(id => {
+                    return <UserContainer user={id} />
+                })
+            }
+        </div>
     )
 }
 
