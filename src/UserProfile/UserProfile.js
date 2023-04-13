a246f147636eb3cac2c69e06bbfb332f1d5b065f
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-13 19:41:12 +0300

    UserProfile.css lisätty

--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -1,6 +1,8 @@
+import './UserProfile.css'
 import { useLocation } from "react-router-dom";
 import { UserContainer } from "../components/UserContainer";
 import { useState, useEffect } from "react";
+import CardGroup from 'react-bootstrap/CardGroup';
 
 const UserProfile = () => {
     const search = useLocation().search;
@@ -19,12 +21,16 @@ const UserProfile = () => {
     },[]);
 
     return (
-        <div>
-            {
-                containerData.filter(item => item.id == auctionId).map(id => {
-                    return <UserContainer user={id} />
-                })
-            }
+        <div className='main-container'>
+            <div className='user-container'>
+                <CardGroup>
+                {
+                    containerData.filter(item => item.id == auctionId).map(id => {
+                        return <UserContainer user={id} />
+                    })
+                }
+                </CardGroup>
+            </div>
         </div>
     )
 }
